a922203df95ddb69874da7efbc9b8ee66654eb51
An249 <145014268+An249@users.noreply.github.com>
2025-02-21 12:52:52 +0530

    Add files via upload

---
+++ src/suby/data.js
@@ -0,0 +1,29 @@
+export const itemData = [
+  {
+    item_img: "/assets/item/ChickenBiriyani.jpg",
+  },
+  {
+    item_img: "/assets/item/pizza.jpg",
+  },
+  {
+    item_img: "/assets/item/Idli.jpg",
+  },
+  {
+    item_img: "/assets/item/Juice.jpg",
+  },
+  {
+    item_img: "/assets/item/Pancake.jpg",
+  },
+  {
+    item_img: "/assets/item/Paratha.jpg",
+  },
+  {
+    item_img: "/assets/item/Poha.jpg",
+  },
+  {
+    item_img: "/assets/item/Poori.jpg",
+  },
+  {
+    item_img: "/assets/item/Vada.jpg",
+  },
+];
